66d51b9bdd7afc43b26f4f477fcc2d3cb8b436c6
Oscar Tinajero <tinajero117.oscar@gmail.com>
2022-08-30 15:17:15 -0500

    Se agrega el código de régimen

--- lib/app/data/models/caracteristicas_fiscales.dart
+++ lib/app/data/models/caracteristicas_fiscales.dart
@@ -2,33 +2,39 @@ class CaracteristicasFiscales {
   CaracteristicasFiscales({
     required this.regimen,
     required this.fechaAlta,
+    required this.codeRegimen,
   });
 
   final String regimen;
   final String fechaAlta;
+  final int codeRegimen;
 
   CaracteristicasFiscales copyWith({
     String? regimen,
     String? fechaAlta,
+    int? codeRegimen,
   }) =>
       CaracteristicasFiscales(
         regimen: regimen ?? this.regimen,
         fechaAlta: fechaAlta ?? this.fechaAlta,
+        codeRegimen: codeRegimen ?? this.codeRegimen,
       );
 
   factory CaracteristicasFiscales.fromJson(Map<String, dynamic> json) =>
       CaracteristicasFiscales(
-        regimen: json["regimen"],
-        fechaAlta: json["fecha_alta"],
-      );
+          regimen: json["regimen"],
+          fechaAlta: json["fecha_alta"],
+          codeRegimen: json["code_regimen"]);
 
   Map<String, dynamic> toJson() => {
         "regimen": regimen,
         "fecha_alta": fechaAlta,
+        "code_regimen": codeRegimen,
       };
 
   factory CaracteristicasFiscales.getDefault() => CaracteristicasFiscales(
         regimen: '',
         fechaAlta: '',
+        codeRegimen: 0,
       );
 }
