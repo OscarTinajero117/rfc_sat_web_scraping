d628049f25b7a43de88b595835d2dedc3e7f20b3
Oscar Tinajero <tinajero117.oscar@gmail.com>
2022-09-01 13:28:59 -0500

    Cambios ligeros

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../global_widgets/load_splash.dart';
 import '../../global_widgets/rounded_button.dart';
 import 'home_controller.dart';
 
@@ -10,47 +9,48 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => controller.loading
-        ? const LoadSplash(mensaje: 'Cargando')
-        : Scaffold(
-            appBar: AppBar(
-              title: const Center(
-                child: Text(
-                  'RFC por QR',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              leading: const SizedBox(width: 5.0),
-              actions: [
-                IconButton(
-                  onPressed: controller.toAbout,
-                  icon: const Icon(Icons.info),
-                ),
-                const SizedBox(width: 5.0),
-              ],
-            ),
-            body: Container(
-              alignment: Alignment.center,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const Center(
-                      child: Text(
-                        'Escanea el código QR\nque viene en la constancia\nde situación fiscal',
-                        textAlign: TextAlign.center,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'RFC por QR',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        leading: const SizedBox(width: 5.0),
+        actions: [
+          IconButton(
+            onPressed: controller.toAbout,
+            icon: const Icon(Icons.info),
+          ),
+          const SizedBox(width: 5.0),
+        ],
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Obx(
+            () => controller.loading
+                ? const CircularProgressIndicator.adaptive()
+                : Column(
+                    children: [
+                      const Center(
+                        child: Text(
+                          'Escanea el código QR\nque viene en la constancia\nde situación fiscal',
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
-                    RoundedButton(
-                      text: 'Escanear QR',
-                      onPressed: () {
-                        FocusScope.of(context).unfocus();
-                        controller.scanQR();
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ));
+                      RoundedButton(
+                        text: 'Escanear QR',
+                        onPressed: () {
+                          controller.scanQR();
+                        },
+                      ),
+                    ],
+                  ),
+          ),
+        ),
+      ),
+    );
   }
 }
